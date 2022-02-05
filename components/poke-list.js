ff6532409d63b543604a41215a9b0b552e13669c
Alejandro Cerezo <alce65@hotmail.es>
2022-02-05 19:31:42 +0100

    Add Home page

--- components/poke-list.js
+++ components/poke-list.js
@@ -2,8 +2,10 @@ import { Component } from './component.js';
 
 export class PokeList extends Component {
     #template;
-    constructor(selector) {
+    #pokeList;
+    constructor(selector, pokelist) {
         super();
+        this.#pokeList = pokelist;
         this.#template = this.#createTemplate();
         this.render(selector, this.#template);
     }
@@ -11,11 +13,37 @@ export class PokeList extends Component {
     #createTemplate() {
         let template = `
             <h2>Lista de Pokemos</h2>
-            <div class="poke-list">`;
-        template += `
-        <a class="poke-item__link" href='./detail.html?id=1'>un pokemon</a>
+            <ul class="poke-list__list">`;
+        this.#pokeList.forEach((poke) => {
+            template += `
+                <li class="poke-item">
+                    <a class="poke-item__link" href='./detail.html?id=${poke.id}'>
+                        <span class="poke-item__link-label">${poke.name}</span>
+                        <span class="poke-item__link-sprite">
+                            <img class="poke-item__link-sprite-front"
+                            src="${poke.sprites.front_default}" alt="${poke.name}">
+                            <img class="poke-item__link-sprite-back"
+                            src="${poke.sprites.back_default}" alt="${poke.name}">
+                        </span>
+                    </a>
+                    <span class="poke-item__fav">
+                     <i class="icon--score far fa-heart"></i>
+                    </span>
+                </li>`;
+        });
+
+        template += `</ul>
+            <div class="pagination">
+                <button class="pagination__button" type="button">
+                    <i class="fas fa-backward"></i>
+                    <span>Anterior</span>
+                </button>
+                <button class="pagination__button" type="button">
+                    <span>Siguiente</span>
+                    <i class="fas fa-forward"></i>
+                </button>
+            </div>
         `;
-        template += `</div>`;
         return template;
     }
 }
--- js/app.js
+++ js/app.js
@@ -3,15 +3,18 @@ import { Header } from '../components/header.js';
 import { MyPokeList } from '../components/my-poke-list.js';
 import { PokeDetail } from '../components/poke-detail.js';
 import { PokeList } from '../components/poke-list.js';
+import { URL_POKE_API } from './config.js';
 
-function app() {
+async function app() {
+    const pokeData = await hydrateData();
+    console.log({ pokeData });
     console.log('Index loaded');
     new Header('.header');
     console.log(location.pathname);
     switch (location.pathname) {
         case '/':
         case '/index.html':
-            new PokeList('.poke-list');
+            new PokeList('.poke-list', pokeData);
             break;
         case '/my-pokemons.html':
             new MyPokeList('.my-poke-list');
@@ -21,6 +24,43 @@ function app() {
             break;
     }
     new Footer('.footer');
+}
+
+async function hydrateData() {
+    const initialPokeList = await fetchPoke(URL_POKE_API);
+    console.log(initialPokeList);
+
+    const fullPokes = await Promise.all(
+        initialPokeList.results.map(async (item) => {
+            const pokeData = await fetchPoke(item.url);
+            return pokeData;
+        })
+    );
+
+    console.log(fullPokes);
+
+    return initialPokeList.results.map((item) => {
+        const id = item.url.split('/').at(-2);
+        const sprites = fullPokes.find((poke) => {
+            return poke.name === item.name;
+        })?.sprites;
+        return { ...item, id: id, sprites: sprites };
+    });
+
+    /*return initialPokeList.results.reduce(async (prev, item) => {
+         const id = item.url.split('/').at(-2);
+        const pokeData = await fetchPoke(item.url);
+        console.log(pokeData);
+        await prev;
+        return [...prev, { ...item, id: id, sprites: pokeData.sprites }];
+    }, []); */
+}
+
+async function fetchPoke(url) {
+    const resp = await fetch(url, {
+        mode: 'cors',
+    });
+    return resp.json();
 }
 
 document.addEventListener('DOMContentLoaded', app);
